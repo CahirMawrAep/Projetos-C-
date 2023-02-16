7509d76f7c90e204044e70f1fde02f65ddc3beb6
Willian Inacio <wilhelmviw@gmail.com>
2023-02-15 23:09:54 -0300

    Banco

---
+++ Banco/Cadastrar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+namespace Banco
+{
+    class Cadastrar
+    {
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double ContaBancaria { get; private set; }
+
+        public Cadastrar(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Cadastrar(int numero, string titular, double contaBancaria) : this(numero, titular)
+        {
+            ContaBancaria = contaBancaria;
+        }
+
+        public void Deposito(double quantia)
+        {
+            ContaBancaria += quantia;
+        }
+
+        public void Saque(double saque)
+        {
+            ContaBancaria -= (saque + 5.00);
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + ContaBancaria.ToString("F2", CultureInfo.InvariantCulture) ; 
+        }
+
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+namespace Banco
+{
+    internal class Banco
+    {
+        static void Main(string[] args)
+        {
+            Cadastrar conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string depOpt = Console.ReadLine();
+
+            if ( depOpt == "s" || depOpt == "S")
+            {
+                Console.Write("Entre um valor para depósito: ");
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Cadastrar(numero, titular, valor);
+
+            }
+
+            else
+            {
+                conta = new Cadastrar(numero, titular);
+            }
+            Console.WriteLine("\r\n Dados da Conta");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para depósito: ");
+            double valorr = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(valorr);
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para saque: ");
+            valorr = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(valorr);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            
+
+        }
+    }
+}
